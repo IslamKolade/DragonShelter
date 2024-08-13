a01205e6abacbf360ab3d6aa17896fcad9689111
IslamKolade <islamsanni66@gmail.com>
2024-08-13 04:55:59 +0100

    Debugging 500 error

--- DragonShelter/settings.py
+++ DragonShelter/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)@ni1+)!22#gz8pntxsxjdb1eewv=(2jv2%ba0ik6vs!q@lk(+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.thedragonshelter.com', '.vercel.app', 'localhost', '127.0.0.1']
 
