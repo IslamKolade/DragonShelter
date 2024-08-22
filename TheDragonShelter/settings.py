f29d0d1161375331fa41107c167658c6a18db84f
IslamKolade <islamsanni66@gmail.com>
2024-08-22 09:03:42 +0100

    Removed vercel.app from ALLOWED HOSTS

--- TheDragonShelter/settings.py
+++ TheDragonShelter/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 #DEBUG = True
 
-ALLOWED_HOSTS = ['www.thedragonshelter.com', '.vercel.app', 'dragonshelter.onrender.com',  'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.thedragonshelter.com', 'dragonshelter.onrender.com',  'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://www.thedragonshelter.com/']
 
